fe221e8b932c43038b1d8a67f7b2724509e8d132
101851011281 <10185101281@stu.ecnu.edu.cn>
2026-08-06 15:50:28 +0800

    Link breakdown RPC shutdown export

--- app/nvmf_tgt/semiraid_latency_breakdown_recorder.c
+++ app/nvmf_tgt/semiraid_latency_breakdown_recorder.c
@@ -1,2 +1,3 @@
 /* Evaluation-only recorder implementation; selected by CONFIG_SEMIRAID_LATENCY_BREAKDOWN. */
 #include "../../../../test/shared/latency_breakdown/src/semiraid_latency_breakdown.c"
+#include "../../../../test/shared/latency_breakdown/src/semiraid_latency_breakdown_rpc.c"
